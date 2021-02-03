e899080ac49ba09efd0920d80b2e2800b772c8b4
Aidan Grafenauer Parker <aidantgp@gmail.com>
2021-02-03 20:19:41 +1100

    Its functional!!! im happy wit it
    > Bug if you make a line too long
    > Make the input button do stuff
    > make noodles
    > comment and tidy all code
    > drop down menus for theme and language
    > color picker but do this last because its dumb

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,23 @@
 //<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-function import_code() {
-    //Import user code
-}
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    viewportMargin: Infinity,
+    theme: "material-darker",
+    mode: "python",
+    lineWrapping: true
+  });
 
-function export_code() {
-    //Export user code
-
-    let user_code = document.getElementById('editor').innerText;
-    $.ajax({
-        url: "http://localhost:5000/export/",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify({"code": user_code})
-    }).done(function(data) {
-        console.log(data);
-    });
-}
+function export_code() { 
+    let div = document.getElementById('container');
+    html2canvas(div, {
+        backgroundColor: null
+    }).then( 
+        function (canvas) { 
+            image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            var link = document.createElement('a');
+            link.download = "nice-code.png";
+            link.href = image;
+            link.click();
+        }) 
+} 
 

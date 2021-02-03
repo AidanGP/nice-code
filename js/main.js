7272d21bb28db52bfdaea1214b8d35180632d1e2
Aidan Grafenauer Parker <aidantgp@gmail.com>
2021-02-03 21:29:32 +1100

    2/3 evening session done (no flask!!!)
    >Changed some styling
    >Made is so that the editor is automatically in focus on load.
    >Commented and tidied some js

--- js/main.js
+++ js/main.js
@@ -3,18 +3,22 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     viewportMargin: Infinity,
     theme: "material-darker",
     mode: "python",
-    lineWrapping: true
+    lineWrapping: true,
+    autofocus: true
   });
 
+function import_code() {
+    // OnClick function for uploading code.
+    // Probably obsolete because of the drag and drop feature.
+}
 
 
 function export_code() {
     // OnClick function for downloading the submitted code.
     let div = document.getElementById('container');
     html2canvas(div, {
-        backgroundColor: null
-    }).then( 
-        function (canvas) { 
+        backgroundColor: null,
+    }).then(function (canvas) { 
             image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
             var link = document.createElement('a');
             link.download = "nice-code.png";

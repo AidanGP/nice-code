1072b890cfff42a3e02d45f4a013d6104ec63ce5
Aidan Grafenauer Parker <aidantgp@gmail.com>
2021-02-04 17:02:41 +1100

    4/2 school day changes
    > stopped using a pre-downloaded version of codemirror
    > downloading when code is run instead
    > added theme and mode selectors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
 // Initialise the CodeMirror editor
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     viewportMargin: Infinity,
-    theme: "material-darker",
-    mode: "python",
+    theme: "monokai",
+    mode: "javascript",
     lineWrapping: true,
     autofocus: true
-  });
+});
+
+var lang = document.getElementById("language");
+var mode = ["xml","javascript","css","htmlmixed","python"];
+
+for (var i = 0; i < mode.length; i ++) {
+    var item = mode[i];
+    var element = document.createElement("option");
+    element.textContent = item;
+    element.value = item;
+    lang.appendChild(element);
+}
+
+var theme = document.getElementById("theme");
+var themes = ["3024-day", "3024-night", "ambiance", "base16-dark", "base16-light", "blackboard",
+          "cobalt", "eclipse", "elegant", "erlang-dark", "lesser-dark", "midnight", "monokai",
+          "neat"];
+
+for (var i = 0; i < themes.length; i ++) {
+    var item = themes[i];
+    var element = document.createElement("option");
+    element.textContent = item;
+    element.value = item;
+    theme.appendChild(element);
+}
+
+function changeMode(obj) {
+    var val = obj.value;
+    editor.setOption("mode", val);
+    console.log(val);
+}
+
+function changeTheme(obj) {
+    var val = obj.value;
+    editor.setOption("theme", val);
+    console.log(val);
+}
 
 function import_code() {
     // OnClick function for uploading code.
